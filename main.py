9eed77f482c3f6a4e8eec3b6acb53e76a20b44e8
arafet <marnissiarafet@gmail.com>
2024-08-20 09:55:35 +0100

    data ingestion added

---
+++ main.py
@@ -0,0 +1,18 @@
+from Customer_churn.logging import logger
+from Customer_churn.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+
+    data_ingestion = DataIngestionTrainingPipeline()
+
+    data_ingestion.main()
+
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/Customer_churn/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,16 @@
+from Customer_churn.config.configuration import ConfigurationManager
+from Customer_churn.components.data_ingestion import DataIngestion
+from Customer_churn.logging import logger
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+        
